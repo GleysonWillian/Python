16d55878608513f141d411dfee47d2f6af3a5235
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-01-11 18:06:05 -0300

    Operadores de comparação e lógicos

---
+++ comparacao.py
@@ -0,0 +1,33 @@
+num1 = 11
+num2 = 11
+
+#if num1 == num2:
+    #print("Os números são iguais!")
+#if num1 != num2:
+    #print("Os números são diferentes!")
+#if num1 > num2:
+    #print("O número 1 é maior que o número 2!")
+#if num1 < num2:
+    #print("O número 2 é maior que o número 1!")
+if num1 <= num2:
+    print("O número 2 é maior ou igual ao número 1!")
+elif num1 >= num2:
+    print("O número 1 é maior ou igual ao número 2!")
+
+# Operadores de comparação em Python:
+# - Igual: ==
+# - Diferente: !=
+# - Maior: >
+# - Menor: <
+# - Maior igual: >=
+# - Menor igual: <=
+
+# Operadores lógicos em Python:
+# - Or: ou
+# - And: e
+# - Not(): negação
+
+# Operadores:
+# - If: se
+# - Else: senão
+# - Elif: senão, se
