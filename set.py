e87e66a1bbecba2181cd51faa0e6a73b5f0a3e3a
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-01-12 16:15:38 -0300

    Set Python

---
+++ set.py
@@ -0,0 +1,22 @@
+# É uma lista desordenada e não aceita valores duplicados;
+# É mutável, mas os valores adicionados não podem são mutáveis;
+# O set usa chaves {}. A lista usa [];
+# Não tem índice
+# Consigo adicionar e remover valores, mas nunca mudar um valor por outro
+
+#frutas = {"Maça", "Laranja", "Banana"}
+
+#frutas.add ("Pera") # Adicionar
+
+#frutas.remove ("Maça") # Remover 
+
+#rutas.pop () # Remove o último valor
+
+#print(frutas)
+
+set1 = {"Maça","Laranja","Banana"}
+set2 = {0,1,300,15,26}
+set3 = {True,False,False,True}
+set4 = {"Gleyson",24,True}
+
+print (set4)
