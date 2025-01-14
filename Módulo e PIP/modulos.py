84c59081e9237bfddca3c7f3adf4674c4b4602af
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-01-14 16:54:45 -0300

    Módulos e PIP - Python

---
+++ Módulo e PIP/modulos.py
@@ -0,0 +1,11 @@
+# Bibliotecas em qualquer outra linguagem conhecida;
+# São pacotes de funções e variáveis que pode utilizar e reutilizar;
+# Podem ser criados pelo usuários ou aderidas diretamente da biblioteca de módulos do Python ou podem ser de terceiros;
+# Exemplo de módulo: 'os' (pertence ao python).
+
+import tools
+
+print(tools.PI)
+print(tools.GRAVITY)
+print(tools.get_extension("test.txt"))
+print(tools.highest_number([1,2,-560,800,1500,89]))
